357b058cc559d1357d138f8b967d69dfbdb7edfe
Yumingyuan <timdavid@vip.qq.com>
2019-03-12 14:46:10 +0800

    update first step of merge

--- merge_sort.py
+++ merge_sort.py
@@ -27,7 +27,9 @@ def merge(need_sort_list):
 if __name__=='__main__':
 	list_unsort=[2,10,13,18,19,1,3,5,7,9]
 	list_unsort1=[1,3,5,7,9,2,10,13,18,19]
-	list_unsort2=[1,3,5,7,9,2,10,13,18]
+	list_unsort2=[1,3,5,7,9,2,6,10,13,18]
+	list_unsort3=[1,7,8,10,3,4,5,6]
 	merge(list_unsort)
 	merge(list_unsort1)
 	merge(list_unsort2)
+	merge(list_unsort3)
