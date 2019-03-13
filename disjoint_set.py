e842466ad119fd63c7820763ed2b3d1be45a56f5
Yumingyuan <timdavid@vip.qq.com>
2019-03-13 10:27:38 +0800

    insert comment in disjoint_set.py

--- disjoint_set.py
+++ disjoint_set.py
@@ -1,11 +1,11 @@
 def make_set(x):
-	p=[i for i in range(0,x)]
-	print(p)
-	return p
+	p=[i for i in range(0,x)]#generate the dataset
+	rank=[0 for i in range(0,x)]#generate the rank
+	return p,rank
 def find_set(search_set,x):
-	if x>len(search_set)-1:
+	if x>len(search_set)-1:#x is higher than the length of the tree
 		return -1
-	return search_set[x]
+	return search_set[x]#return the find x'th value
 def union(search_set,p,q):
 	pid=p
 	qid=q
@@ -16,7 +16,7 @@ def union(search_set,p,q):
 			search_set[i]=search_set[qid]
 	print("after union:",search_set)
 if __name__=='__main__':
-	gen_set=make_set(10)
+	gen_set,rank=make_set(10)
 	info=input('>[num1] [num2]:')
 	while len(info):
 		split_info=info.split(' ')
