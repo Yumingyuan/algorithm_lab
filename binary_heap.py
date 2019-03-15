d3d678068fd5aef0a55fde3c0dab5240a3079493
Yumingyuan <timdavid@vip.qq.com>
2019-03-15 15:48:58 +0800

    finish def extract_heap_max(search_data):

--- binary_heap.py
+++ binary_heap.py
@@ -44,8 +44,8 @@ def heapify(search_data,i):
 			temp=search_data[i]
 			search_data[i]=search_data[largest]
 			search_data[largest]=temp
-			print("heapify:",i,search_data)
-		heapify(search_data,largest)
+			print("heapify1:",i,search_data)
+			heapify(search_data,largest)#when exchange occur you need heapify
 	elif right_num is None and left_num is not None:
 		if left_num<len(search_data) and search_data[left_num]>search_data[i]:
 			largest=left_num
@@ -55,8 +55,8 @@ def heapify(search_data,i):
 			temp=search_data[i]
 			search_data[i]=search_data[largest]
 			search_data[largest]=temp
-			print("heapify:",i,search_data)
-		heapify(search_data,largest)
+			print("heapify2:",i,search_data)
+			heapify(search_data,largest)#when exchange occur you need heapify
 #build function,to build up a big heap
 def build_heap(search_data):
 	length=len(search_data)
@@ -64,6 +64,14 @@ def build_heap(search_data):
 	mid=int(mid+1)
 	for i in range(mid,0,-1):
 		heapify(search_data,i)
+def extract_heap_max(search_data):
+	length=len(search_data)
+	if length<1:
+		return
+	max_num=search_data[1]
+	search_data[1]=search_data.pop()#get the last item of search_data and  make the list length minor 1
+	heapify(search_data,1)
+	return max_num
 if __name__=="__main__":
 	test_data=[0,16,14,10,8,7,9,3,2,4,1]
 	test_data1=[0,4,5,3,2,7,9,1,11,8,12]
@@ -80,3 +88,6 @@ if __name__=="__main__":
 	heapify(test_data1,1)
 	print("begin build heap:",test_data2)
 	build_heap(test_data2)
+	print('After build heap:',test_data2)
+	print("begin extract the max num from heap!")
+	print("Extract heap:",extract_heap_max(test_data2))
