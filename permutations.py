e8a6b9dd80d75e79035e8b663e4cfc9a7296b01d
Yumingyuan <timdavid@vip.qq.com>
2019-03-20 20:05:37 +0800

    全排列加入中文注释

--- permutations.py
+++ permutations.py
@@ -2,15 +2,14 @@
 #全排列问题
 def Swap(perm_list,swap1,swap2):
 	perm_list[swap2],perm_list[swap1]=perm_list[swap1],perm_list[swap2]
-	#print("swap",perm_list)
 def Perm(perm_list,begin,end):
 	if begin==end:
 		print(perm_list)
 	else:
-		for i in range(begin,end):
-			Swap(perm_list,begin,i)
-			Perm(perm_list,begin+1,end)
-			Swap(perm_list,i,begin)
+		for i in range(begin,end):#对于每一个元素，进行交换并进行递归的perm调用
+			Swap(perm_list,begin,i)#交换i与begin
+			Perm(perm_list,begin+1,end)#继续交换后面的
+			Swap(perm_list,i,begin)#恢复本轮交换的i与begin
 if __name__=="__main__":
 	num=int(input("input the num of permutation:"))
 	list_perm=[i+1 for i in range(num)]
