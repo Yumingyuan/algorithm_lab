2fac957f3f81e967c3e38398a41bff748a5cc68f
Yumingyuan <timdavid@vip.qq.com>
2019-03-26 12:50:47 +0800

    完成霍纳法则的多项式计算方法正向,加入注释

--- horner_calc.py
+++ horner_calc.py
@@ -1,7 +1,7 @@
 def horner(data_list,x):
 	p=data_list[0]#取多项式中次数最高项系数
 	for i in range(1,len(data_list)):
-		p=p*x+data_list[i]
+		p=p*x+data_list[i]#计算:b*x+c
 	return p
 if __name__=="__main__":
 	data_set=[2,-1,3,1,-5]
