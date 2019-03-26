652b58c9fdea1227f92c98e07f156daa7f1de2cc
Yumingyuan <timdavid@vip.qq.com>
2019-03-26 12:47:28 +0800

    完成霍纳法则的多项式计算方法正向

--- horner_calc.py
+++ horner_calc.py
@@ -1,8 +1,8 @@
 def horner(data_list,x):
-	p=data_list[len(data_list)-1]#取多项式中次数最高项系数
-	for i in range(len(data_list)-2,-1,-1):#将每个加数（实际上也是系数）进行相加，由于要访问数组的0项元素，所以边界是-1（python range特性所造成的）
-		p=p*x+data_list[i]#计算:x*p+num
-	return p#返回结果
+	p=data_list[0]#取多项式中次数最高项系数
+	for i in range(1,len(data_list)):
+		p=p*x+data_list[i]
+	return p
 if __name__=="__main__":
-	data_set=[-5,1,3,-1,2]
+	data_set=[2,-1,3,1,-5]
 	print("calc result:",horner(data_set,3))#函数调用
