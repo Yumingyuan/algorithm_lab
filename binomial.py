70c8665c2cbb64ceeba4ea6d680a91fabbabc0e6
Yumingyuan <timdavid@vip.qq.com>
2019-04-01 18:54:23 +0800

    完成二项式系数计算

--- binomial.py
+++ binomial.py
@@ -7,6 +7,7 @@ def binominal_calc(n,k):
 				bin_list[i][j]=1#如果是c(n,0)或c(n,n)则结果为1
 			else:
 				bin_list[i][j]=bin_list[i-1][j-1]+bin_list[i-1][j]#反之则动态规划，使用之前计算的结果进行计算
+	print(bin_list)
 	print(bin_list[n][k])
 if __name__=="__main__":
 	binominal_calc(3,2)#函数调用
