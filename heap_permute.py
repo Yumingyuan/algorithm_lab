1793733f028077bc1eb47f3862bb29f8bcc05c4f
Yumingyuan <timdavid@vip.qq.com>
2019-04-02 15:02:50 +0800

    修改程序正确性

--- heap_permute.py
+++ heap_permute.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 def heap_permute(data_set,n):
 	if n==1:
-		print(data_set[1:5])#切片
+		print(data_set[1:])#切片
 	else:
-		for i in range(1,n):
+		for i in range(1,n+1):#从1到n
 			heap_permute(data_set,n-1)
 			if n%2==1:
 				data_set[1],data_set[n]=data_set[n],data_set[1]#交换元素1与n的位置
@@ -11,5 +11,5 @@ def heap_permute(data_set,n):
 				data_set[i],data_set[n]=data_set[n],data_set[i]#交换元素i与n的位置
 				#heap_permute(data_set,n-1)
 if __name__=='__main__':
-	data=[0,1,2,3]
-	heap_permute(data,3)
+	data=[0,1,2]
+	heap_permute(data,2)
