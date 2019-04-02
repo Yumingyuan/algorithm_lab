731113410ef8b4ce5d6acf7099a331a9bab23b5d
Yumingyuan <timdavid@vip.qq.com>
2019-04-02 14:48:27 +0800

    加入注释

--- heap_permute.py
+++ heap_permute.py
@@ -6,9 +6,9 @@ def heap_permute(data_set,n):
 		for i in range(1,n):
 			heap_permute(data_set,n-1)
 			if n%2==1:
-				data_set[1],data_set[n]=data_set[n],data_set[1]
+				data_set[1],data_set[n]=data_set[n],data_set[1]#交换元素1与n的位置
 			else:
-				data_set[i],data_set[n]=data_set[n],data_set[i]
+				data_set[i],data_set[n]=data_set[n],data_set[i]#交换元素i与n的位置
 				#heap_permute(data_set,n-1)
 if __name__=='__main__':
 	data=[0,1,2,3]
