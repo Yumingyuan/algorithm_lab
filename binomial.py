662855b9600a6945d6c33e811762ba06a338ba64
Yumingyuan <timdavid@vip.qq.com>
2019-04-03 09:06:19 +0800

    完成二项式系数计算

--- binomial.py
+++ binomial.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 def binominal_calc(n,k):
-	bin_list=[[0 for i in range(k+1)] for j in range(n+1)]#生成二维数组的列表生成方法
+	bin_list=[[0 for i in range(k+1)] for j in range(n+1)]#生成二维列表生成方法生成k+1列和n+1行
+	print(bin_list)
 	for i in range(n+1):#从0遍历至n
 		for j in range(k+1):#从0遍历至k
 			if j==0 or j==i:
@@ -8,6 +9,6 @@ def binominal_calc(n,k):
 			else:
 				bin_list[i][j]=bin_list[i-1][j-1]+bin_list[i-1][j]#反之则动态规划，使用之前计算的结果进行计算
 	print(bin_list)
-	print(bin_list[n][k#打印结果
+	print(bin_list[n][k])#打印结果
 if __name__=="__main__":
-	binominal_calc(3,2)#函数调用
+	binominal_calc(3,2)#函数调用,需要生成3行4列
