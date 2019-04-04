5874956939b904f41a8694360710f309260f3d6f
Yumingyuan <timdavid@vip.qq.com>
2019-04-04 10:57:57 +0800

    完成约瑟夫问题递推式版本

---
+++ joseph_problem_recursion.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+def joseph(n):
+	if n==1:
+		return 1
+	elif n%2==0:
+		return 2*joseph(n/2)-1
+	else:
+		return 2*joseph((n-1)/2)+1
+if __name__=="__main__":
+	print("alive",joseph(4))
