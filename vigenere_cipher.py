7595af213606c54a78c098c525f3969cd97997c1
Yumingyuan <timdavid@vip.qq.com>
2019-04-04 14:34:51 +0800

    完成维吉尼亚密码

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -2,7 +2,7 @@
 vigenere_dict_c_to_num={"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,
 "g":6,"h":7,"i":8,"j":9,"k":10,"l":11,"m":12,"n":13,
 "o":14,"p":15,"q":16,"r":17,"s":18,"t":19,"u":20,"v":21,
-"w":22,"x":23,"y":24,"z":25}
+"w":22,"x":23,"y":24,"z":25}#字母转为数字索引字典
 vigenere_list_num_to_c=[chr(i) for i in range(97,123)]#生成26个字母表
 def generate_key(plain_text,key):#用于生成密钥
 	gen_key=""
