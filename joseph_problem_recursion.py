6f241c97cd3128baa851b149419b5a19887f653e
Yumingyuan <timdavid@vip.qq.com>
2019-04-08 14:30:42 +0800

    约瑟夫环递推

--- joseph_problem_recursion.py
+++ joseph_problem_recursion.py
@@ -2,9 +2,9 @@
 def joseph(n):
 	if n==1:
 		return 1
-	elif n%2==0:
+	elif n%2==0:#偶数情况
 		return 2*joseph(n/2)-1
-	else:
+	else:#奇数情况
 		return 2*joseph((n-1)/2)+1
 if __name__=="__main__":
 	print("alive",joseph(5))
