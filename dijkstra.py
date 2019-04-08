ef7ef540ead2702f52d2ab59c4c42bd5bad22b8c
Yumingyuan <timdavid@vip.qq.com>
2019-04-08 16:38:41 +0800

    完成dijkstra算法

--- dijkstra.py
+++ dijkstra.py
@@ -7,8 +7,18 @@ map_data_list=[
 [2,1,1,0,7],
 [65535,65535,3,7,0]
 ]
+map_from_num_to_letter={0:"a",1:"b",2:"c",3:"d",4:"e",5:"f",
+6:"g",7:"h",8:"i",9:"j",10:"k",11:"l",12:"m",13:"n",
+14:"o",15:"p",16:"q",17:"r",18:"s",19:"t",20:"u",21:"v",
+22:"w",23:"x",24:"y",25:"z"}
 min_length=[65535 for i in range(len(map_data_list))]#到已在被选中点集合中距离
 selected=[False for i in range(len(map_data_list))]#判断节点是否已经被选中
+def print_result(node_list):#打印最短路径函数
+	print("经过节点:",end='')
+	for i in range(len(node_list)):
+		print(map_from_num_to_letter[node_list[i]],end='')
+		if i!=len(node_list)-1:
+			print(",",end='')
 if __name__=="__main__":
 	chosen_node=0
 	node_list=[chosen_node]#记录经过节点
@@ -28,4 +38,4 @@ if __name__=="__main__":
 			if min_length[j]>min_length[chosen_node]+map_data_list[chosen_node][j]:
 				min_length[j]=min_length[chosen_node]+map_data_list[chosen_node][j]#更新到原点的最短距离
 	print("min length:",min_length[len(min_length)-1])
-	print("经过节点:",node_list)
+	print_result(node_list)
