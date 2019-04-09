071e5033491c0692df4238aad60cb549ceacad1a
Yumingyuan <timdavid@vip.qq.com>
2019-04-09 16:06:29 +0800

    完成使用两个栈模拟一个队列

--- stack_simulate_queue.py
+++ stack_simulate_queue.py
@@ -6,6 +6,7 @@ def delete_head(stack1,stack2):
 			data=stack1.pop()#取出stack1栈顶元素
 			stack2.append(data)#将stack1的栈顶元素放入stack2
 	if len(stack2)==0:#stack2栈内没东西
+		print("Error! stack2 is empty")
 		return
 	head_num=stack2.pop()
 	print("headnum:",head_num)
@@ -16,8 +17,8 @@ if __name__=="__main__":
 	delete_head(stack1,stack2)
 	stack1.append("e")#往空的栈里再加些东西
 	stack1.append("f")#往空的栈里再加些东西
-	delete_head(stack1,stack2)
-	delete_head(stack1,stack2)
-	delete_head(stack1,stack2)
-	delete_head(stack1,stack2)
-	delete_head(stack1,stack2)
+	delete_head(stack1,stack2)#有效取队列头
+	delete_head(stack1,stack2)#有效取队列头
+	delete_head(stack1,stack2)#有效取队列头
+	delete_head(stack1,stack2)#有效取对猎头
+	delete_head(stack1,stack2)#无效取队列头（已经空了）
