ea30dd3fddf01c8c05c202fdc692b97fa86fa685
Yumingyuan <timdavid@vip.qq.com>
2019-04-12 16:30:17 +0800

    完成矩阵链乘积动态规划

--- matrix_chan_multi.py
+++ matrix_chan_multi.py
@@ -7,11 +7,12 @@ def matrix_chain_order():
 	#当i=1，j=0,k从0-1
 	for i in range(1,matrix_num):#i负责关注的是运算规模，第一轮是计算2个矩阵连乘的状态
 		for j in range(i-1,-1,-1):#j负责取出每一个矩阵的行数
-			matrix_value[j][i]=65535#大值
+			matrix_value[j][i]=655350000000000#非常大的值，确保乘积不会超过
 			for k in range(j,i):#选择在哪里断开括号j<=k<i
 				if matrix_value[j][k]+matrix_value[k+1][i]+chain[j]*chain[k+1]*chain[i+1]<matrix_value[j][i]:
 					matrix_value[j][i]=matrix_value[j][k]+matrix_value[k+1][i]+chain[j]*chain[k+1]*chain[i+1]
 					matrix_solution[j][i]=k#最优断开位置
+				print("k",k,i,j)
 def print_solution(i,j):#从i打印到j
 	if i==j:#索引到单个矩阵时，直接打印
 		print("A"+str(i),end='')#end参数保证print不会换行
