21b82e17994a041fcca4a395a8882144b793b65f
Yumingyuan <timdavid@vip.qq.com>
2019-04-13 19:05:49 +0800

    流水作业调度johnson法则

--- johnson_rules.py
+++ johnson_rules.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+def tekezero(elem):
+	return elem[0]#返回第零个元素		
 def john_calc(job_nums,a,b,c):
-	
+	for i in range
 if __name__=="__main__":
-	a=[2,4,3,6,1]
-	b=[5,2,3,1,7]
+	a=[(0,2),(1,4),(2,3),(3,6),(4,1)]
+	b=[(0,5),(1,2),(2,3),(3,1),(4,7)]
 	sequence=[0 for i in range(len(a))]
