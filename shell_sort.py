61ab9bd2e2b2d225a48af941bbc59f8fe4a43f8d
Yumingyuan <timdavid@vip.qq.com>
2019-04-15 15:34:17 +0800

    希尔排序

--- shell_sort.py
+++ shell_sort.py
@@ -7,20 +7,21 @@ print('Before sort:',list_unsort)
 
 gap=1
 while gap<int(length/3):
-	gap=gap*3+1
+	gap=gap*3+1#计算一个合适的gap值
 
 while gap>=1:
 	print("current gap:",gap)
 	for i in range(int(gap),int(length)):
 		j=i
-		temp=list_unsort[j]
+		temp=list_unsort[j]#获取当前比较位置j的元素下标值
 		print("current compare num:",temp)
-		while j>=gap and list_unsort[j]<list_unsort[j-gap]:
+		while j>=gap and list_unsort[j]<list_unsort[j-gap]:#把当前j与j-gap进行比较
 			list_unsort[j]=list_unsort[j-gap]
 			list_unsort[j-gap]=temp
 			j=j-gap
 			print("medium unsort list:",list_unsort)
 	gap=int(gap/3)
+	print("gap",gap)
 print('After sort:',list_unsort)
 	
 			
