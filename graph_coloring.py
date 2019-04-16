65bd49f977f2c498a5912bfc0d2d5a025d8f6db6
Yumingyuan <timdavid@vip.qq.com>
2019-04-16 17:57:03 +0800

    着色问题

--- graph_coloring.py
+++ graph_coloring.py
@@ -4,7 +4,7 @@ node_num=5
 node_matrix=[[False,True,True,True,False],[True,False,True,True,True],
 [True,True,False,True,False],[True,True,True,False,True],[False,True,False,True,False]]
 def iscollision(node_set,node_color,i,color):
-	for j in range(i):#遍历从i出发的所有临接节点j
+	for j in range(i):#遍历已经着色的节点
 		if node_set[i][j]==True and node_color[j]==color:#如果有一个j与i临接且着色一样，则不行！
 			return True
 	return False#没找到合适的j就没有颜色冲突
