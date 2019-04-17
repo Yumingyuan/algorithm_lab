09f34b9ab1a06c149528f65e5a11cecd746d6403
Yumingyuan <timdavid@vip.qq.com>
2019-04-17 17:24:15 +0800

    矩阵的warshall算法

---
+++ warshall.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+if __name__=="__main__":
+	data_matrix_A=[[False,True,False,False],
+											[False,False,False,True],
+											[False,False,False,False],
+											[True,False,True,False]]
+	
