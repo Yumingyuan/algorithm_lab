3e0a9eb7170034b52a7aeab55e8a796dd9931a41
Yumingyuan <timdavid@vip.qq.com>
2019-04-17 18:46:23 +0800

    回溯法图的3着色问题递归版本

--- 3_coloring-problem.py
+++ 3_coloring-problem.py
@@ -2,6 +2,6 @@
 #临接矩阵
 node_matrix=[[False,True,True,True,False],[True,False,True,True,True],
 [True,True,False,True,False],[True,True,True,False,True],[False,True,False,True,False]]
-
+node_color=[0 for i in range(len(node_matrix))]
 if __name__=="__main__":
-	node_color=[0 for i in range(len(node_matrix))]
+	
