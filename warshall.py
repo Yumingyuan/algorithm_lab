9cdeb82df69a377a0b4603cedae814e926de43d7
Yumingyuan <timdavid@vip.qq.com>
2019-04-17 18:08:31 +0800

    矩阵的warshall算法

--- warshall.py
+++ warshall.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 def warshall_calc(matrix):
 	for edge in range(len(matrix)):
-		pass
-	
+		for start_node_num in range(len(matrix)):
+			for end_node_num in range(len(matrix)):
+				if matrix[start_node_num][end_node_num]==True:#上一轮矩阵元素就是True时，不做操作
+					pass
+
 if __name__=="__main__":
 	data_matrix_A=[[False,True,False,False],
 											[False,False,False,True],
