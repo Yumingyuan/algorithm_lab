150da202025abd9f469de135f67b868489fb5929
Yumingyuan <timdavid@vip.qq.com>
2019-04-17 19:01:00 +0800

    回溯法图的3着色问题递归版本

--- 3_coloring-problem.py
+++ 3_coloring-problem.py
@@ -13,8 +13,9 @@ def graphcolor(procedure):
 	if procedure==len(node_matrix):
 		print("resolution:",node_color)
 	else:
-		for i in range(1,4):
-			node_color[procedure]=i
-			
+		for color in range(1,5):
+			node_color[procedure]=color
+			if not iscollision(node_matrix,node_color,procedure,color):
+				graphcolor(procedure+1)
 if __name__=="__main__":
-	
+	graphcolor(0)
