6c2989ad529ebd236c618f66fd21bb01cb115223
Yumingyuan <timdavid@vip.qq.com>
2019-04-18 21:08:47 +0800

    最优二叉查找树分析

--- optimal_binary_search_tree.py
+++ optimal_binary_search_tree.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 probablity=[0.1,0.2,0.4,0.3]
-solution=[[0 for i in range(len(probablity)+1)] for j in range(len(probablity)+2)]#生成节点个数+2行，节点个数+1列
+#生成节点个数+2行，节点个数+1列
+solution=[[0 for i in range(len(probablity)+1)] for j in range(len(probablity)+2)]
 
