6e2714815523853aa0ab7129e36718dbe9bd700e
Yumingyuan <timdavid@vip.qq.com>
2019-04-19 09:08:20 +0800

    最优二叉查找树分析

--- optimal_binary_search_tree.py
+++ optimal_binary_search_tree.py
@@ -7,7 +7,6 @@ result=[[0 for i in range(len(probablity)+1)] for j in range(len(probablity)+1)]
 def initial():
 	for i in range(len(probablity)):
 		solution[i+1][i+1]=probablity[i]#查找“i+1”这个点的单个概率
-	#print("after init",solution)
 def calc_optimal():
 	min_k=0
 	for dimension in range(1,len(probablity)):
@@ -16,20 +15,16 @@ def calc_optimal():
 			min_val=65535
 			for k in range(i,j+1):#决定谁当父节点
 				if solution[i][k-1]+solution[k+1][j]<min_val:
-					#print("get in if:",solution[i][k-1],solution[k+1][j],k)
 					min_val=solution[i][k-1]+solution[k+1][j]#更新最优值
 					min_k=k
 			solution[i][j]=min_val#更新最优值
 			result[i][j]=min_k#更新最优断开位置k
 			sum_prob=0
-			for index in range(i-1,j):#从i-1加到j-1
-				#print("add",probablity[index])
+			for index in range(i-1,j):#从i-1加到j-1把所有节点的查找概率加入
 				sum_prob+=probablity[index]
-			#print("After add ",sum_prob)
-			#print("soltion:",solution)
 			solution[i][j]+=sum_prob
 	print(solution[1][len(probablity)])
-	print(result)
+	print_result(1,4)
 if __name__=="__main__":
 	initial()#调用初始化函数
 	calc_optimal()
