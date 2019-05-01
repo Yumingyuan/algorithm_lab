58e23fd42a4bfe5ee2bcbe70f8e6f154e0aa22d9
Yumingyuan <timdavid@vip.qq.com>
2019-05-01 17:16:02 +0800

    多边形划分动态规划算法

--- polygon_divide.py
+++ polygon_divide.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
-
+#计算三角形周长
+def triangle(edge_data,i,j,k):
+	return edge_data[i][k]+edge_data[k][j]+edge_data[i][j]
+#最优值计算函数
+def calc_optimal(edge_data,edges_num):
+	distance=[[0 for i in range(edges_num+1)] for j in range(edges_num+1)]#最优子权重
+	for m in range(1,edges_num):#问题规模
+		for i in range(0,edges_num-m):
+			j=i+m
+			min_num=65535
+			min_k=65535
+			for k in range(i+1,k):
+				if min_num>distance[i][k]+distance[k][j]+triangle(i,j,k):
+					min_num=distance[i][k]+distance[k][j]+triangle(edge_data,i,j,k)
+					min_k=k
+			distance[i][j]=min_num
+		
 if __name__=="__main__":
 	edge_data=[[0,14,25,27,10,11,24,16],
 	[0,0,18,15,27,28,16,14],
