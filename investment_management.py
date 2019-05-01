c1ae8f318e13b4851ab44baec48c7f919a9a9bd3
Yumingyuan <timdavid@vip.qq.com>
2019-05-01 09:51:56 +0800

    投资项目动态规划求解

--- investment_management.py
+++ investment_management.py
@@ -2,13 +2,21 @@
 #动态规划计算最优值函数
 def calc_optimal(project_data,project_num,money):
 	#计算最优投资额的向量
-	selection_income=[[0 for i in rang(money+1)] for j in range(project_num+1)]#多生成一个投资8万也会盈利0元的项目作为边缘的计算变量
+	selection_income=[[0 for i in range(money+1)] for j in range(project_num+1)]#多生成一个投资8万也会盈利0元的项目作为边缘的计算变量
+	#记录最优投资额的向量
+	optimum_investment=[0 for i in range(project_num)]
 	#计算过程，反向计算
 	for i in range(project_num-1,-1,-1):#从2号项目到0号项目
-		for j in range(0,money+1):#从0万到8万,决定投资i项目所花的资金
+		for j in range(0,money+1):#从0万到8万,决定投资i及i以后项目所花的资金
 			max_num=0#小值
-			if max_num<project_data[i][j]+selection_income[i+1][money-j]:
-				max_num=project_data[i][j]+selection_income[i+1][money-j]#实时最优值
+			opti_invest_cur=0#记录当前阶段最优投资额
+			opti_invest_next=0#记录后续项目由于本阶段投资剩余投资金额数目
+			for k in range(0,j):#决定投资i项目注入资金
+				if max_num<project_data[i][k]+selection_income[i+1][j-k]:
+					max_num=project_data[i][k]+selection_income[i+1][j-k]#实时最优值
+					opti_invest_cur=k
+					opti_invest_next=j-k
+			
 			
 if __name__=="__main__":
 	#投资金额及收回的金额，下标为投资金额，盈利为数据
