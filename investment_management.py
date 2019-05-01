29d583b33b342e16b1f0a9511f0c52f4014044bd
Yumingyuan <timdavid@vip.qq.com>
2019-05-01 09:58:16 +0800

    投资项目动态规划求解

--- investment_management.py
+++ investment_management.py
@@ -16,13 +16,14 @@ def calc_optimal(project_data,project_num,money):
 					max_num=project_data[i][k]+selection_income[i+1][j-k]#实时最优值
 					opti_invest_cur=k
 					opti_invest_next=j-k
-			
-			
+			selection_income[i][j]=max_num
+	print(selection_income)									
 if __name__=="__main__":
 	#投资金额及收回的金额，下标为投资金额，盈利为数据
 	project_num=3
 	investment_money=8
-	projec_data=[
-	[0,5,15,40,80,90.95.98,100],
+	project_data=[
+	[0,5,15,40,80,90.95,98,100],
 	[0,5,15,40,60,70,73,74,75],
 	[0,4,26,40,45,50,51,52,53]]
+	calc_optimal(project_data,project_num,investment_money)
