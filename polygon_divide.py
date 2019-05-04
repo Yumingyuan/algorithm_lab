ae408b8258bfcd36f918e597cfe26aef597dae6e
Yumingyuan <timdavid@vip.qq.com>
2019-05-04 11:45:30 +0800

    多边形最优计算

--- polygon_divide.py
+++ polygon_divide.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-#计算三角形周长
+#计算并返回三角形周长(i,j,k这三个点形成的)
 def triangle(edge_data,i,j,k):
 	return edge_data[i][k]+edge_data[k][j]+edge_data[i][j]
 #最优值计算函数
 def calc_optimal(edge_data,edges_num):
-	distance=[[0 for i in range(edges_num+1)] for j in range(edges_num+1)]#最优子权重
+	distance=[[0 for i in range(edges_num)] for j in range(edges_num)]#最优子权重
+	optim_k=[[0 for i in range(edges_num)] for j in range(edges_num)]#最优的分割点
 	for m in range(1,edges_num):#问题规模
 		for i in range(0,edges_num-m):
 			j=i+m
@@ -13,12 +14,14 @@ def calc_optimal(edge_data,edges_num):
 			min_k=0
 			#print("distance:",distance)
 			for k in range(i+1,j):
-				#print("distance[i][k]",distance[i][k],distance[k][j],triangle(edge_data,i,j,k),i,j,k)
+				#print("distance[i][k]:",distance[i][k],"distance[k][j]",distance[k][j],triangle(edge_data,i,j,k),i,j,k)
 				if min_num>distance[i][k]+distance[k][j]+triangle(edge_data,i,j,k):
 					min_num=distance[i][k]+distance[k][j]+triangle(edge_data,i,j,k)
 					min_k=k
-				distance[i][j]=min_num
-	print(distance)
+				distance[i][j]=min_num	
+				optim_k[i][j]=min_k			
+	print("optimum weight:",distance[0][edges_num-1])
+	#print(optim_k)
 if __name__=="__main__":
 	edge_data=[[0,14,25,27,10,11,24,16],
 	[0,0,18,15,27,28,16,14],
