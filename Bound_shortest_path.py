80eac5ca9130bacf53ddd7889568e665971ffa20
Yumingyuan <timdavid@vip.qq.com>
2019-05-08 22:49:49 +0800

    分支定界法

--- Bound_shortest_path.py
+++ Bound_shortest_path.py
@@ -8,5 +8,10 @@ map_data_list=[
 length=[0,65535,65535,65535,65535]
 Queue=[]#用于进行分支的队列
 def branch(matrix,v0):
-	pass
+	Queue.append(v0)
+	current_vertex=matrix[0]
+	while len(Queue)!=0:
+		head=Queue[0]#取出当前队手需要搜索的节点下标
+		for i in range(len(current_vertex)):
+			if length[head]+current_vertex[i]
 	
