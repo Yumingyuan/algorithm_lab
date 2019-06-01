8400c869165bf909c42ebe85e3b415d6b3ad525f
Yumingyuan <timdavid@vip.qq.com>
2019-06-01 15:54:24 +0800

    作业2分支限界的城市最小花费问题

--- Bound_Road_assignment2/Bound_Road.py
+++ Bound_Road_assignment2/Bound_Road.py
@@ -3,10 +3,13 @@ def read_file():
 	f1_list=[]#公路联通情况和每段公路的长度
 	f2_list=[]#公路养护费用
 	f1=open("m1.txt","r")
-	
+	f2=open("m2.txt","r")
 	for lines in f1:
 		ls=lines.strip('\n').split('\t')#去掉结尾的\n并把每行内容按\t分开
 		f1_list.append(ls)
-	print(f1_list)
+	for lines in f2:
+		ls=lines.strip('\n').split('\t')#去掉结尾的\n并把每行内容按\t分开
+		f2_list.append(ls)
+	return f1_list,f2_list
 if __name__=="__main__":
 	read_file()
