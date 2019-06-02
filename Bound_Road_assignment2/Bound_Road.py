4ae1aa0ccca7465c3d79bfff6239c835aec76125
Yumingyuan <timdavid@vip.qq.com>
2019-06-02 14:14:46 +0800

    作业2分支限界的城市最小花费问题

--- Bound_Road_assignment2/Bound_Road.py
+++ Bound_Road_assignment2/Bound_Road.py
@@ -2,33 +2,16 @@
 #上界：当前已经符合要求的最小解
 #下界：没有金额约束的最优最短路
 import copy
-path=[]#当前路径
-path.append(0)#一定经过起始点
-optimal_path=[]#最优路径
-path_length=0#当前路径的长度
-optimal_path_length=9999#符合要求的最优路径长度
-cost=0#当前路径的养路费
-optimal_cost=1500#符合要求的最优养路费（上界）
-def check_optimal(i):#判断剪支和最优
-	if length_list[path[-1]][i]==9999:#当亲扩展节点到i不可达
-		return False
-	if  length_list[path[-1]][i]+path_length+small_dis[i]>optimal_path_length:#当前扩展节点到i点的距离加上目前的最短路长度加从i到终点的距离大于最优值
-		return False
-	if value_list[path[-1]][i]+cost+small_cos[i]>1500:#当前扩展节点到i点的距离加上目前的最短路花费以及到终点花费大于最优值，肯定不能要了
-		return False
-	return True
 def bound_search():
-	global length_list
-	global value_list
-	global small_dis
-	global small_cos
+	global length_list#正常的临接长度
+	global value_list#正常的临接花费
+	global small_dis#所有点到乙最短距离
+	global small_cos#所有点到乙最小花费
 	global path#当前路径
-	global cost#当前话费
+	global cost#当前花费
 	global optimal_cost
 	global optimal_path
 	global path_length
-	global length_list
-	global value_list
 	global optimal_path_length
 	if path[-1]==49:#如果当前扩展节点到达乙了(最后一个元素为49点)
 		#更新
@@ -41,15 +24,23 @@ def bound_search():
 		return
 	#print(path)
 	for i in range(49):
-		j=49-i#从最后点往前扩充
-		if check_optimal(j)==True:
-			path_length+=	min_list_length[path[-1]][j]
-			cost+=min_cost_length[path[-1]][j]
+		j=49-i#从最后点往前扩充i:0->48,j:49->1
+		if check_optimal(j):
+			path_length+=	length_list[path[-1]][j]
+			cost+=value_list[path[-1]][j]
 			path.append(j)
 			bound_search()
 			path.pop()
-			path_length+=	min_list_length[path[-1]][j]
-			cost+=min_cost_length[path[-1]][j]
+			path_length-=	length_list[path[-1]][j]
+			cost-=value_list[path[-1]][j]
+def check_optimal(i):#判断剪支和最优
+	if length_list[path[-1]][i]==9999:#当亲扩展节点到i不可达
+		return False
+	if  length_list[path[-1]][i]+path_length+small_dis[i]>optimal_path_length:#当前扩展节点到i点的距离加上目前的最短路长度加从i到终点的距离大于最优值
+		return False
+	if value_list[path[-1]][i]+cost+small_cos[i]>optimal_cost:#当前扩展节点到i点的距离加上目前的最短路花费以及到终点花费大于最优值，肯定不能要了
+		return False
+	return True
 def read_file():#读取文件函数,返回公路长度和养护费用列表
 	f1_list=[]#公路联通情况和每段公路的长度
 	f2_list=[]#公路养护费用
@@ -80,12 +71,22 @@ def to_yi_smallest_list(min_list_length):#所有节点到乙的距离
 	to_yi_small_list.append(0)#乙到自己的距离和花费都为0
 	return to_yi_small_list
 if __name__=="__main__":
+	path=[]#当前路径
+	path.append(0)#一定经过起始点
+	optimal_path=[]#最优路径
+	path_length=0#当前路径的长度
+	optimal_path_length=9999#符合要求的最优路径长度
+	cost=0#当前路径的养路费
+	optimal_cost=1500#符合要求的最优养路费（上界）
 	length_list,value_list=read_file()#长度数据和花费数据
 	#print("Before soft:",value_list)
 	min_list_length=floyd(length_list)
+	#print(min_list_length)
 	min_cost_length=floyd(value_list)
 	small_dis=to_yi_smallest_list(min_list_length)#所有点到乙的最短距离
 	small_cos=to_yi_smallest_list(min_cost_length)#所有点到乙的最小花费
+	#print(small_dis)
+	#print(small_cos)
 	bound_search()
 	print(optimal_path)
 	print(optimal_path_length)
