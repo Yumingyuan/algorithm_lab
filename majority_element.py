a88598eb5b86d5737abfae193f48d2620999034e
Yumingyuan <timdavid@vip.qq.com>
2019-06-07 17:06:06 +0800

    搜索多数元素

---
+++ majority_element.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+data=[1,3,2,3,3,4,3]
+if __name__=="__main__":
+	num=data[0]
+	count=0
+	for i in data:
+		if i==num:
+			count+=1
+		else:
+			count-=1
+			if count<0:
+				num=i
+				count=0
+	print("majority item:",num)
