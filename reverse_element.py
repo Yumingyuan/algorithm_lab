01bdbbf874c8fb14d244c36d5bad09328e8b4879
Yumingyuan <timdavid@vip.qq.com>
2019-06-19 17:44:15 +0800

    求逆序数

--- reverse_element.py
+++ reverse_element.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+items=[1,2,0,4,3,2,6]
+queue=[]
+count=0
+for i in range(len(items)):
+	for j in range(i,len(items)):
+		if items[i]>items[j]:
+			count+=1
+			queue.append((items[i],items[j]))
+print("count result:",count)
+print("result:",queue)
