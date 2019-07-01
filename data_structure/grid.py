2969df5dff4aca43fa4e8ab578cf4b0b28ad61b6
Yumingyuan <timdavid@vip.qq.com>
2019-07-01 20:26:46 +0800

    二维数组

--- data_structure/grid.py
+++ data_structure/grid.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 from arrays import Array
-
+class Grid(object):
+	def __init__(self,rows,columns,fillValue=None):
+		self.data=Array(rows)#初始化行
+		for row in range(rows):
+			self.data[row]=Array(columns,fillValue)#初始化列
+			
+	def getHeight(self):
+		return len(self.data)
+	def getWidth(self):
+		return len(self.data[0])
