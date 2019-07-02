759a89ce4f18626d7d41cb077e191d7a745affbc
Yumingyuan <timdavid@vip.qq.com>
2019-07-02 19:00:16 +0800

    链表测试

--- data_structure/Node.py
+++ data_structure/Node.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+class Node(object):
+	def __init__(self,data,next=None):
+		self.data=data
+		self.next=next
+if __name__=="__main__":
+	node1=None
+	node2=Node("data1",None)
+	node3=Node("data2",node2)
+	print(node3.next.data)#访问node3的下一个元素的存储内容
