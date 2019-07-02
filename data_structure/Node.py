aa376bfd645ab44df3ac886aba5d4df065853f24
Yumingyuan <timdavid@vip.qq.com>
2019-07-02 19:07:03 +0800

    链表测试

--- data_structure/Node.py
+++ data_structure/Node.py
@@ -4,13 +4,14 @@ class Node(object):
 		self.data=data
 		self.next=next
 def test_node():
+	head=None
 	for count in range(1,6):
 		head=Node('data'+str(count),head)
-	return head
+	return head#返回头节点
 def iter_node(head):
 	while head!=None:
 		print(head.data)
 		head=head.next
 if __name__=="__main__":
 	head_node=test_node()
-	iter_note(head_node)
+	iter_node(head_node)
