099beb17f3a42ba1d930721f0b632f9ca8cca7d7
Yumingyuan <timdavid@vip.qq.com>
2019-07-03 16:17:29 +0800

    链表测试

--- data_structure/Node.py
+++ data_structure/Node.py
@@ -3,16 +3,17 @@ class Node(object):
 	def __init__(self,data,next=None):
 		self.data=data
 		self.next=next
-def test_node():#创建链表函数
+def test_node():#创建链表函数(头部插入法)
 	head=None
 	for count in range(1,6):
 		head=Node('data'+str(count),head)
 		print("create node",head)
 	return head#返回头节点
 def iter_node(head):#遍历链表
-	while head!=None:
-		print(head.data)
-		head=head.next
+	probe=head#获取头指针位置
+	while probe!=None:
+		print(probe.data+" ",end='')
+		probe=probe.next
 if __name__=="__main__":
 	head_node=test_node()
 	print("iter node!")
