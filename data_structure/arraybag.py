0df83d570b1c7f4505c05052fbd0373b37e299a1
Yumingyuan <timdavid@vip.qq.com>
2019-07-24 18:05:49 +0800

    基于数组实现的背包

--- data_structure/arraybag.py
+++ data_structure/arraybag.py
@@ -2,4 +2,12 @@ from arrays import Array
 class ArrayBag(object):
 	DEFAULT_CAPACITY=10#默认长度
 	def __init__(self,sourceCollection=None):
-		self.item=Array(DEFAULT_CAPACITY)#初始化Array对象
+		self.item=Array(ArrayBag.DEFAULT_CAPACITY)#初始化Array对象
+		self.size=0
+		if sourceCollection:
+			for i in sourceCollection:
+				self.item.add(i)
+if __name__=="__main__":
+	hi=[1,3,4,5,7]
+	a=ArrayBag(hi)
+	print(a)
